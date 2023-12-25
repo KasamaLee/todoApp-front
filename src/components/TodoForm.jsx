802cf7d6b257885bd00a6d8ee9f91db562188624
KasamaLee <k.kasama.lee@gmail.com>
2023-12-25 14:36:12 +0700

    create task

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,31 +1,51 @@
+import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
 
 export default function TodoForm() {
 
-    const [taskInput, setTaskInput] = useState([])
+    const [taskInput, setTaskInput] = useState('')
 
     const handleInputChange = (e) => {
         setTaskInput(e.target.value)
     }
     console.log(taskInput)
 
+    const handleCreateTask = async () => {
+        if (taskInput.trim() === '') {
+            alert('Please add your task')
+        }
+
+        const request = {
+            text: taskInput
+        }
+        
+        const response = await axios.post('http://localhost:7777/todo/create', request)
+        setTaskInput('')
+        // console.log(response)
+    }
+
     return (
-        <form>
-            <div className='relative'>
-                <input
-                    id='text'
-                    placeholder='+ Add new task'
-                    type='text'
-                    value={taskInput}
-                    maxLength={40}
-                    className='ring-2 ring-gray-400 rounded p-3 w-full'
-                    onChange={handleInputChange}
-                />
 
-                <button className='absolute right-2 top-1 text-white px-4 py-2 bg-green-500 rounded-3xl flex justify-center items-center gap-1 hover:opacity-50'>Add</button>
-            </div>
+        <div className='relative'>
+            <input
+                id='text'
+                placeholder='+ Add new task'
+                type='text'
+                value={taskInput}
+                maxLength={40}
+                className='ring-2 ring-gray-400 rounded p-3 w-full'
+                onChange={handleInputChange}
+            />
 
-        </form>
+            <button
+                className='absolute right-2 top-1 text-white px-4 py-2 bg-green-500 rounded-3xl flex justify-center items-center gap-1 hover:opacity-50'
+                onClick={handleCreateTask}
+            >
+                Add
+            </button>
+        </div>
+
+
     )
 }
